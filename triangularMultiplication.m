aa090e859fd91c2a8f8afab9af77c184a7ef183d
Cosmo <cosmo_chan@126.com>
2017-04-25 20:57:17 +0800

    triagngular multiplication

--- triangularMultiplication.m
+++ triangularMultiplication.m
@@ -1,9 +1,8 @@
-function C = matrixMultiplication(A, B, version, C)
+function C = triangularMultiplication(A, B)
 %% --------------- Interface ---------------
 %   Input
-%       A : matrix, size = (m, p)
-%       B : matrix, size = (p, n)
-%       C : matrix, size = (m, n)
+%       A : matrix, size = (n, n)
+%       B : matrix, size = (n, n)
 %       version : int
 %               Specify the version of matrixMultiplication. 
 %               VERSION may be one of the following int:
@@ -12,7 +11,7 @@ function C = matrixMultiplication(A, B, version, C)
 %                   '1.1.7' : Saxpy version(algorithm 1.1.7)
 %                   '1.1.8' : Outer product version(algorithm 1.1.8)
 %   Output
-%       C = AB + C
+%       C = AB
 %   Example
 %       >> A = [1 2; 3 4; 5 6;]
 %       >> B = [7 8 9 10; 11 12 13 14]
@@ -23,45 +22,20 @@ function C = matrixMultiplication(A, B, version, C)
 %       >> tic; C3 = matrixMultiplication(A, B, C, '1.1.7'), toc;
 %       >> tic; C4 = matrixMultiplication(A, B, C, '1.1.8'), toc;
 %   Reference
-%       Algorithm 1.1.5-1.1.8
+%       Algorithm 1.2.1
 %       Golub G H, Van Loan C F. Matrix computations[M]. JHU Press, 2012.
 %   Coder
 %       Weizhe Chen : cosmo_chan@126.com
 %% -----------------------------------------
-    [m, p] = size(A);
-    [~, n] = size(B);
-    if nargin < 3
-        C = zeros(m, n);
-        version = '1.1.8';
-    elseif nargin < 4
-        C = zeros(m, n);
-    end
+    [n, n] = size(A);
+    C = zeros(n, n);
 %% -----------------------------------------
-    if strcmp(version, '1.1.5')
-        for i = 1 : m
-            for j = 1 : n
-                 for k = 1 : p
-                     C(i, j) = A(i, k) * B(k, j) + C(i, j);
-                 end
+    for i = 1 : n
+        for j = i : n
+            for k = i : j
+                C(i, j) = A(i, k) * B(k, j) + C(i, j);
             end
         end
-    elseif strcmp(version, '1.1.6')
-        for i = 1 : m
-            for j = 1 : n
-                C(i, j) = A(i, :) * B(:, j) + C(i, j);
-            end
-        end
-    elseif strcmp(version, '1.1.7')
-        for j = 1 : n
-            for k = 1 : p
-                C(:, j) = saxpy(B(k, j), A(:, k), C(:, j));
-            end
-        end
-    elseif strcmp(version, '1.1.8')
-        for k = 1 : p
-            C = outerProductUpdate(C, A(:, k), B(k, :));
-        end
-    else
-        warning('VERSION must be string "1.1.5", "1.1.6", "1.1.7" or "1.1.8"')
     end
+    
 end
